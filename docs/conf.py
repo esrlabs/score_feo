27b8f538ddfdbb63e3c0ef42ed827ad44ac5901f
armin.bartsch <armin.bartsch@accenture.com>
2025-09-08 14:55:51 +0200

    Adapting docs/conf.py
    Change-Id: Ibed0201c2f0ca7593067325367aad25042d59576

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Module Template Project"
-project_url = "https://eclipse-score.github.io/module_template/"
-project_prefix = "MODULE_TEMPLATE_"
+project = "FEO Framework"
+project_url = "https://eclipse-score.github.io/feo/"
+project_prefix = "FEO_"
 author = "S-CORE"
 version = "0.1"
 
