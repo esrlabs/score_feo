1eda01bf6615ba19bf902f9496754e1e6454f2bc
Oliver Mueller <oliver.mueller@esrlabs.com>
2025-11-07 10:35:00 +0100

    Add prost toolchain to feo (#9)
    Change-Id: Iefb99599109aeb4ca33c14df1ef2777a1c3f01de
    
    Co-authored-by: Dominik Fischer <dominik.a.fischer@accenture.com>

--- perfetto-model/src/lib.rs
+++ perfetto-model/src/lib.rs
@@ -10,11 +10,4 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
-
-#[allow(clippy::all)]
-#[rustfmt::skip]
-mod perfetto {
-    include!(concat!(env!("OUT_DIR"), "/perfetto.protos.rs"));
-}
-
-pub use perfetto::*;
+pub use perfetto_proto::perfetto::protos::*;
